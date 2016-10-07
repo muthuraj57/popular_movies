cc49a291346fb396eb9b50d7df020d67d8a3622d
muthuraj57 <muthu.sattur@gmail.com>
2016-10-08 00:20:06 +0530

    gradle files added

---
+++ app/src/main/java/com/movies/popularmovies/util/GenerateUrl.java
@@ -0,0 +1,10 @@
+/* $Id$ */
+package com.movies.popularmovies.util;
+
+/**
+ * Created by muthu-3955 on 07/10/16.
+ */
+public class GenerateUrl {
+    private static final String API_KEY = "7d0900783de7f8c19a366a3b17bd0083";
+    public static final String API_READ_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDA5MDA3ODNkZTdmOGMxOWEzNjZhM2IxN2JkMDA4MyIsInN1YiI6IjU3ZjdkNmJiYzNhMzY4MjNkMTAwMjU1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JXN-ttLfk6dc6Rd4_v0EGkvg23xIOu0xatZqHVmaYEs";
+}
